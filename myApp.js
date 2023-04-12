e1dccc1d89db6e66492e443453effa068318c2ed
Sheikhman_Kalderon <duah229@outlook.com>
2023-04-12 19:09:43 +0000

    Retrying

--- myApp.js
+++ myApp.js
@@ -4,8 +4,7 @@ let app = express();
 console.log("Hello World");
 
 app.get("/", (req,res) => {
-    res.sendFile(__dirname + "/views/index.html" );
-    //res.send("Hello Express");
+    res.sendFile(__dirname + "/views/index.html");
 });
 app.use(express.static(__dirname + "/public"));
 app.use("/public", express.static(__dirname + "/public"));
