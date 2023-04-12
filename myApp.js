038399fff029711d5c509079b533527612c5b506
Sheikhman_Kalderon <duah229@outlook.com>
2023-04-12 00:34:11 +0000

    Printing to the console wrt for express

--- myApp.js
+++ myApp.js
@@ -2,6 +2,11 @@ let express = require('express');
 let app = express();
 console.log("Hello World");
 
+const outConsole = (req,res) => {
+    res.send("Hello Express");
+};
+app.get("/",outConsole);
+
 
 
 
