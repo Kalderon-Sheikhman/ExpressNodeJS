3aec32b5783a66fefc8df3d312ec7beb41be9c23
Sheikhman_Kalderon <duah229@outlook.com>
2023-04-12 00:58:11 +0000

    Middleware and mounting with app.use

--- myApp.js
+++ myApp.js
@@ -6,7 +6,8 @@ app.get("/", (req,res) => {
     res.sendFile(__dirname + "/views/index.html" );
     //res.send("Hello Express");
 });
-
+app.use(express.static(__dirname + "/public"));
+app.use("/public", express.static(__dirname + "public"));
 
 
 
